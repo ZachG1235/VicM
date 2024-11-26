db0a7020147d32bcffd983fe9223205e70bbb64b
Devin-Jay <dds324@nau.edu>
2024-11-26 02:19:24 -0700

    did transitions between scenes

--- VicM/Assets/Scripts/Entrance.cs
+++ VicM/Assets/Scripts/Entrance.cs
@@ -1,29 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Entrance : MonoBehaviour
 {
     public int nextRoomNumber;
-    public Vector3 nextPos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector3 vicsNextPos;
 
     void OnCollisionEnter2D(Collision2D collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
-            GameManager.SGameManager.ChangeRoom(nextRoomNumber, nextPos);
+            GameManager.SGameManager.ChangeRoom(nextRoomNumber, vicsNextPos);
         }
     }
 }
